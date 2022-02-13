23f233f5be105823a2f7d1cf8e77c37861d461ed
Lucian Duta <duta39@gmail.com>
2022-02-13 19:31:14 +0000

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import getWeb3 from "./getWeb3";
+
+import "./App.css";
+
+class App extends Component {
+  /*
+  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const instance = new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+  runExample = async () => {
+    const { accounts, contract } = this.state;
+
+    // Stores a given value, 5 by default.
+    await contract.methods.set(5).send({ from: accounts[0] });
+
+    // Get the value from the contract to prove it worked.
+    const response = await contract.methods.get().call();
+
+    // Update state with the result.
+    this.setState({ storageValue: response });
+  };
+*/
+  render() {
+    /*
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
+    */
+    return (
+      <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">FLAMEAL</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#transefer">Transfer</Nav.Link>
+            <Nav.Link href="#inventory">My Inventory</Nav.Link>
+            <NavDropdown title="Statistics" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#foodbanks">Food Banks</NavDropdown.Item>
+              <NavDropdown.Item href="#stats">User Statistics</NavDropdown.Item>
+              <NavDropdown.Item href="#contributors">Top Contributors</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="£about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
