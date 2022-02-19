b835effc2eebfcc89a2c3148ba8d044e13315eac
Lucian Duta <duta39@gmail.com>
2022-02-19 13:10:44 +0000

    Improved mobile view

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,74 +1,119 @@
-import React, {useState} from 'react'
-import {Button} from '../Button/Button'
-import './Navbar.css'
-import {Link} from 'react-router-dom'
-import Dropdown from '../Dropdown/Dropdown'
+import React, { useState } from "react";
+import { Button } from "../Button/Button";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Dropdown from "../Dropdown/Dropdown";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const [dropdown, setDropdown] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
 
-    const handleClick = () => setClick(!click);
+  const handleClick = () => setClick(!click);
 
-    const closeMobileMenu = () => setClick(false);
+  const closeMobileMenu = () => setClick(false);
 
-    const onMouseEnter = () => {
-        if(window.innerWidth < 960) {
-            setDropdown(false);
-        } else {
-            setDropdown(true);
-        }
-    };
+  const onMouseEnter = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(true);
+    }
+  };
 
-    const onMouseLeave = () => {
-        if(window.innerWidth < 960) {
-            setDropdown(false);
-        } else {
-            setDropdown(false);
-        }
-    };
+  const onMouseLeave = () => {
+    if (window.innerWidth < 960) {
+      setDropdown(false);
+    } else {
+      setDropdown(false);
+    }
+  };
 
-    return(
-        <>
-            <nav className='navbar'>
-                <Link to = "/" className='navbar-logo'>Flameal</Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className = {click ? 'fas fa-times' : 'fas fa-bars'} />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    <li className='nav-item'>
-                        <Link to = "/transfer" className='nav-links' onClick={closeMobileMenu}>
-                            Transfer
-                        </Link>
-                    </li>
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo">
+          Flameal
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link
+              to="/transfer"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Transfer
+            </Link>
+          </li>
 
-                    <li className='nav-item'>
-                        <Link to = "/myinventory" className='nav-links' onClick={closeMobileMenu}>
-                            My Inventory
-                        </Link>
-                    </li>
+          <li className="nav-item">
+            <Link
+              to="/myinventory"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              My Inventory
+            </Link>
+          </li>
 
-                    <li className='nav-item'
-                        onMouseEnter={onMouseEnter}
-                        onMouseLeave={onMouseLeave}
-                    >
-                        <Link to = "/stats" className='nav-links' onClick={closeMobileMenu}>
-                            Stats <i className = 'fas fa-caret-down' />
-                        </Link>
-                        {dropdown && <Dropdown/>}
-                    </li>
+          <li
+            className="nav-item-opt"
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+          >
+            <Link to="/stats" className="nav-links" onClick={closeMobileMenu}>
+              Stats <i className="fas fa-caret-down" />
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
 
-                    <li className='nav-item'>
-                        <Link to = "/signup" className='nav-links-mobile' onClick={closeMobileMenu}>
-                            Sign Up
-                        </Link>
-                    </li>
-                </ul>
-                <Button />
-            </nav>
-        </>
-        )
+          <li className="nav-item-drop">
+            <Link
+              to="/foodbanks"
+              className="nav-links-drop"
+              onClick={closeMobileMenu}
+            >
+              Food Banks
+            </Link>
+          </li>
+
+          <li className="nav-item-drop">
+            <Link
+              to="/userstats"
+              className="nav-links-drop"
+              onClick={closeMobileMenu}
+            >
+              User Statistics
+            </Link>
+          </li>
+
+          <li className="nav-item-drop">
+            <Link
+              to="/topcont"
+              className="nav-links-drop"
+              onClick={closeMobileMenu}
+            >
+              Top Contributors
+            </Link>
+          </li>
+
+          <li className="nav-item">
+            <Link
+              to="/signup"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+        <Button />
+      </nav>
+    </>
+  );
 }
 
 export default Navbar;
