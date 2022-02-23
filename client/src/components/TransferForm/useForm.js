e464865f79766d83494b7fa0718b3a2e408054f9
Lucian Duta <duta39@gmail.com>
2022-02-23 22:00:59 +0000

    Progress on implementing the final obj

--- client/src/components/TransferForm/useForm.js
+++ client/src/components/TransferForm/useForm.js
@@ -1,18 +1,21 @@
 import { useState, useEffect } from "react";
 import { items } from "../List/List.js";
-
+import useListv2 from "../Listv2Beta/useListv2";
 const useForm = (callback, validate) => {
   const [values, setValues] = useState({
     address: "",
     comments: "",
-    items: [],
+    itemsList: [],
   });
+  let { items } = useListv2();
   const [errors, setErrors] = useState({});
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
+    //alert(JSON.stringify(items));
+    console.log(items);
     setValues({
       ...values,
       [name]: value,
