f3f9866165d927109f75e2c50971136d39f05ef4
Lucian Duta <duta39@gmail.com>
2022-03-05 11:00:15 +0000

    Calling the getData function on page load

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import getData from "./pages/Transfer/getData";
 //*If you ever feel useless...this about this index page
 ReactDOM.render(<App />, document.getElementById("root"));
+
+getData()
+  .then(() => {
+    // alert("Connection with web3 succesfull");
+  })
+  .catch((error) => {
+    alert(
+      `Failed to load web3, accounts, or contract. Check console for details.`
+    );
+    console.log(error);
+  });
