75ce8b66107684827b6375a25322a7bda890a1f6
Lucian Duta <duta39@gmail.com>
2022-03-07 01:32:06 +0000

    Added comments and updated displayed name

--- client/src/components/Button/Button.js
+++ client/src/components/Button/Button.js
@@ -2,42 +2,14 @@ import React from "react";
 import "./Button.css";
 import { Link } from "react-router-dom";
 /**
- *
- * @returns
+ * *Button
+ * *Function to display the button component in the navbar
+ * @returns - A link to the login page and the button element
  */
 export function Button() {
   return (
     <Link to="signup">
-      <button className="btn">Sign Up</button>
+      <button className="btn">Login</button>
     </Link>
   );
 }
-
-/*
-const STYLES = [
-    'btn--primary',
-    'btn--outline'
-]
-
-const SIZES = [
-    'btn--medium',
-    'btn--large'
-]
-
-export const Button = ({
-    children,
-    type,
-    onClick,
-    buttonStyle,
-    buttonSize
-}) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle: STYLES[0]
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize: SIZES[0]
-
-    return (
-        <button className = {`btn ${checkButtonStyle} ${checkButtonSize}`} onClick = {onClick} type={type} >
-            {children}
-        </button>
-        )
-}
-*/
