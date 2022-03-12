d216e88dfd7a98a9cf0f5464fbe2c263dc9e4d08
Lucian Duta <duta39@gmail.com>
2022-03-12 17:36:50 +0000

    Updated the TopCont component accordingly

--- client/src/components/TopContributors/TopContLoading.js
+++ client/src/components/TopContributors/TopContLoading.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
 
+/**
+ * *TopContLoading
+ * * Used to display a loading element while the data is fetched
+ * @returns - a react component
+ */
 const TopContLoading = () => {
   return (
     <div className={"topcont"}>
