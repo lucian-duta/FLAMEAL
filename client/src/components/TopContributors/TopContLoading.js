944e7dbc253801e8308a866eba242bb8c377ed22
Lucian Duta <duta39@gmail.com>
2022-03-12 17:35:30 +0000

    Components for the top cont page

---
+++ client/src/components/TopContributors/TopContLoading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+
+const TopContLoading = () => {
+  return (
+    <div className={"topcont"}>
+      <ClimbingBoxLoader />
+    </div>
+  );
+};
+
+export default TopContLoading;
