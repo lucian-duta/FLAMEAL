c9c216112ea0d71a4bd8ba8206ed38edd5ace0a9
Lucian Duta <duta39@gmail.com>
2022-03-13 02:05:59 +0000

    Implemented the monthly bar chart

--- client/src/components/Statistics/StatsBar.js
+++ client/src/components/Statistics/StatsBar.js
@@ -1,14 +1,17 @@
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
-  } from 'chart.js';
-  import { Bar } from 'react-chartjs-2';
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import React from "react";
+import { fetchPeopleAidedLM } from "../../Web3/extractFeatures";
 
+export function StatsBar() {
   ChartJS.register(
     CategoryScale,
     LinearScale,
@@ -17,38 +20,72 @@ import {
     Tooltip,
     Legend
   );
-  
-  export const options = {
+
+  const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const,
+        display: false,
+        position: "top",
       },
       title: {
-        display: true,
-        text: 'Chart.js Bar Chart',
+        display: false,
+        text: "People aided",
+      },
+    },
+    scales: {
+      y: {
+        ticks: {
+          color: "#d4d4d4",
+          fontSize: 18,
+          stepSize: 100,
+          beginAtZero: true,
+        },
+      },
+
+      x: {
+        ticks: {
+          color: "#d4d4d4",
+          fontSize: 14,
+
+          beginAtZero: true,
+        },
       },
     },
   };
-  
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
-  
-  export const data = {
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December ",
+  ];
+
+  const data = {
     labels,
     datasets: [
       {
-        label: 'Dataset 1',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        backgroundColor: 'rgba(255, 99, 132, 0.5)',
-      },
-      {
-        label: 'Dataset 2',
-        data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
-        backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        label: "Dataset 1",
+        data: labels.map((label) => {
+          if (label === "March") {
+            console.log("DSADASDASDASDdsA", fetchPeopleAidedLM());
+            return fetchPeopleAidedLM();
+          }
+          console.log("labbb", label);
+          return Math.floor(Math.random() * 1000);
+        }),
+        backgroundColor: "rgba(0, 232, 236)",
       },
     ],
   };
-  
-  export function StatsBar() {
-    return <Bar options={options} data={data} />;
-  }
+
+  return <Bar options={options} data={data} />;
+}
