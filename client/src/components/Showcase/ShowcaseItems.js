4c56246e730a0117798e9fb5a06589806ab37341
Lucian Duta <duta39@gmail.com>
2022-03-15 01:42:19 +0000

    Multiplied sample elements

--- client/src/components/Showcase/ShowcaseItems.js
+++ client/src/components/Showcase/ShowcaseItems.js
@@ -29,13 +29,106 @@ const ShowcaseItems = () => {
           </div>
         </div>
       </div>
-      <div className="showcase-card">sal</div>
-      <div className="showcase-card">sal</div>
-      <div className="showcase-card">sal</div>
-      <div className="showcase-card">sal</div>
-      <div className="showcase-card">sal</div>
-      <div className="showcase-card">sal</div>
-      <div className="showcase-card">sal</div>
+      <div className="showcase-card">
+        <div className="card-image">
+          <img
+            src={window.location.origin + "/food-bank.png"}
+            width="350px"
+            height="180px"
+          />
+        </div>
+        <div className="card-text">
+          <h1>Lorem Ipsum</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            fringilla diam eu ligula mattis dictum. Nulla ultricies ante eget
+            purus posuere, eget dapibus magna aliquam. Sed vitae
+          </p>
+        </div>
+        <div className="card-links">
+          <div className="card-links-address">
+            <h1>Address:</h1>
+          </div>
+          <div className="card-links-trans">
+            <h1>Transactions:</h1>
+          </div>
+        </div>
+      </div>
+      <div className="showcase-card">
+        <div className="card-image">
+          <img
+            src={window.location.origin + "/food-bank.png"}
+            width="350px"
+            height="180px"
+          />
+        </div>
+        <div className="card-text">
+          <h1>Lorem Ipsum</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            fringilla diam eu ligula mattis dictum. Nulla ultricies ante eget
+            purus posuere, eget dapibus magna aliquam. Sed vitae
+          </p>
+        </div>
+        <div className="card-links">
+          <div className="card-links-address">
+            <h1>Address:</h1>
+          </div>
+          <div className="card-links-trans">
+            <h1>Transactions:</h1>
+          </div>
+        </div>
+      </div>
+      <div className="showcase-card">
+        <div className="card-image">
+          <img
+            src={window.location.origin + "/food-bank.png"}
+            width="350px"
+            height="180px"
+          />
+        </div>
+        <div className="card-text">
+          <h1>Lorem Ipsum</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            fringilla diam eu ligula mattis dictum. Nulla ultricies ante eget
+            purus posuere, eget dapibus magna aliquam. Sed vitae
+          </p>
+        </div>
+        <div className="card-links">
+          <div className="card-links-address">
+            <h1>Address:</h1>
+          </div>
+          <div className="card-links-trans">
+            <h1>Transactions:</h1>
+          </div>
+        </div>
+      </div>
+      <div className="showcase-card">
+        <div className="card-image">
+          <img
+            src={window.location.origin + "/food-bank.png"}
+            width="350px"
+            height="180px"
+          />
+        </div>
+        <div className="card-text">
+          <h1>Lorem Ipsum</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            fringilla diam eu ligula mattis dictum. Nulla ultricies ante eget
+            purus posuere, eget dapibus magna aliquam. Sed vitae
+          </p>
+        </div>
+        <div className="card-links">
+          <div className="card-links-address">
+            <h1>Address:</h1>
+          </div>
+          <div className="card-links-trans">
+            <h1>Transactions:</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
