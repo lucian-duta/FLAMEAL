bead97954260fed362c84a1c646b31e1302dffe0
Lucian Duta <duta39@gmail.com>
2022-03-21 00:17:03 +0000

    Implemented global state controller

---
+++ client/src/context/UserContext.js
@@ -0,0 +1,51 @@
+import { func } from "prop-types";
+import React, { useState, useContext, useReducer, createContext } from "react";
+
+const checkState = () => {
+  if (!localStorage.getItem("state")) {
+    const initialState = {
+      auth: false,
+      address: null,
+      inventory: [],
+    };
+    return initialState;
+  } else {
+    return JSON.parse(localStorage.getItem("state"));
+  }
+};
+
+const UserContext = createContext(checkState());
+
+const { Provider } = UserContext;
+const reducers = (state, action) => {
+  switch (action.type) {
+    case "auth":
+      return {
+        ...state,
+        auth: true,
+      };
+
+    case "address":
+      return {
+        ...state,
+        address: action.payload,
+      };
+    case "add_to_inv":
+      return {
+        inventory: [...state.inventory, action.payload],
+      };
+    case "rm_from_inv":
+      return {
+        inventory: state.inventory.filter((item) => item.id !== action.payload),
+      };
+    default:
+      throw new Error();
+  }
+};
+const StateProvider = ({ children }) => {
+  const [state, dispach] = useReducer(reducers, checkState());
+
+  return <Provider value={[state, dispach]}>{children}</Provider>;
+};
+
+export { UserContext, StateProvider };
