593788b268d94c90d5ced0410ae4316190b0d5b1
Lucian Duta <duta39@gmail.com>
2022-03-21 00:15:28 +0000

    Added database controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,81 @@
+import UserAccount from "../models/userAccount.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import buffer from "buffer";
+const nonceGenerator = () => {
+  var text = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 16; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+export const getUsers = async (req, res) => {
+  try {
+    const user = await UserAccount.find();
+    console.log(user);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createUser = async (req, res) => {
+  const user = {
+    publicAddress: req.body.publicAddress,
+    nonce: nonceGenerator(),
+  };
+  //!NEED TO CHECK IF EXISTS
+  const newUser = new UserAccount(user);
+
+  try {
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  const body = req.params;
+  console.log(body);
+  try {
+    let user = await UserAccount.findOne({ publicAddress: body.address });
+    // const token = jwt.sign({ publicAddress: body.publicAddress }, "secret123");
+    res.status(200).json(user.nonce);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const authUser = async (req, res) => {
+  const address = req.body.address;
+  const decodedAddress = req.body.decodedAdd;
+  console.log(address);
+  console.log(decodedAddress);
+  try {
+    let user = await UserAccount.findOne({ publicAddress: address });
+    let testAdd = user.publicAddress.toLowerCase();
+    console.log("testAdd: ", testAdd);
+    if (decodedAddress === testAdd) {
+      console.log(testAdd);
+      let update = await UserAccount.updateOne(
+        { publicAddress: address },
+        { nonce: nonceGenerator() }
+      );
+      if (update.acknowledged) {
+        const token = jwt.sign(
+          { publicAddress: user.publicAddress },
+          "secret123"
+        );
+        res.status(200).json(token);
+      }
+    } else {
+      res.status(404).json("failed");
+    }
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
