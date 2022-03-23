0ba06ea11c733dac3666cdc4decbea7934b08ab0
Lucian Duta <duta39@gmail.com>
2022-03-23 22:37:13 +0000

    Fetching foodbanks on load

--- client/src/components/Showcase/Showcase.js
+++ client/src/components/Showcase/Showcase.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getfb } from "../../api/actions";
 import "./Showcase.css";
 import ShowcaseItems from "./ShowcaseItems";
 import { useHorizontalScroll } from "./useSideScroll";
 const Showcase = () => {
   const scrollRef = useHorizontalScroll();
+  const [foodBanks, setFoodBanks] = useState([]);
+  useEffect(() => {
+    getfb()
+      .then((res) => {
+        setFoodBanks(res);
+      })
+      .catch((e) => {
+        setFoodBanks(e);
+      });
+  }, []);
 
   return (
     <>
       <div ref={scrollRef} className="showcase-container">
-        <ShowcaseItems />
+        <ShowcaseItems foodbanks={foodBanks} />
       </div>
     </>
   );
