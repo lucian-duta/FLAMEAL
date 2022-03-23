5a6553a8161d6d50cc4ece8cda9d74d6e15d11b0
Lucian Duta <duta39@gmail.com>
2022-03-23 01:07:16 +0000

    Created foodbank controller

---
+++ server/controllers/foodbank.js
@@ -0,0 +1,38 @@
+import FoodBank from "../models/foodBanks.js";
+
+export const getFB = async (req, res) => {
+  try {
+    const fb = await UserAccount.find();
+    res.status(200).json(fb);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createFB = async (req, res) => {
+  const fb = {
+    fbName: req.body.name,
+    fbDescription: req.body.description,
+    fbAddress: req.body.address,
+    fbPic: req.body.pic,
+  };
+
+  // *frontend payload
+  // payload = {
+  // name: null,
+  // description: null,
+  // address:null,
+  // fbPic: null
+  // }
+
+  console.log("PROPOSED FB: ", fb);
+  //!NEED TO CHECK IF EXISTS
+  const newFB = new UserAccount(user);
+
+  try {
+    await newFB.save();
+    res.status(201).json(newFB);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
+};
