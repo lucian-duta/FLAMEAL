df9be0fdc67f29d55446a909e45d448f76683be9
Lucian Duta <duta39@gmail.com>
2022-03-23 00:17:01 +0000

    Introduced the register user scenario

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,23 +1,96 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { checkUser } from "../../api/actions";
 import { UserContext } from "../../context/UserContext";
+import { fetchAddress } from "../../Web3/getData";
+import Checkbox from "../Checkbox/Checkbox";
+import Popup from "../Popup/Pupup";
 import "./Login.css";
 import LoginLogic from "./LoginLogic";
+import RegisterInput from "./RegisterInput";
+
+/**
+ * * Login
+ * * the react component to handle the login or register of a user
+ * @returns the component
+ */
 const Login = () => {
+  //import the login function for web3 auth
   const { handleLogin } = LoginLogic();
+  //read the global state
   const [state, dispach] = useContext(UserContext);
-  console.log(state.auth);
+  //temporaly store the state in localstorage (to prevent loss on reload)
   localStorage.setItem("state", JSON.stringify(state));
+  //hooks to hold the organisation name and foodbank state (in case of new user)
+  const [orgName, setOrgName] = useState("");
+  const [isFoodBank, setisFoodbank] = useState(false);
+  //hook to hold the the state of user input component
+  const [nameComp, setNameComp] = useState();
+
+  //the callbank function used in the child component
+  const retrieveInfo = (info) => {
+    setOrgName(info.name);
+    setisFoodbank(info.isFB);
+  };
+
+  useEffect(() => {
+    //using the api to check if the user has an account in registered
+    checkUser(fetchAddress())
+      .then(() => {
+        //if the user has an account
+        //don't show the input component
+        setNameComp("");
+      })
+      .catch(() => {
+        //if the user does not have an accunt
+        //show user input
+        setNameComp(
+          <>
+            <RegisterInput sendInfo={retrieveInfo} />
+          </>
+        );
+      });
+  }, [state.auth]);
+
   return (
-    <div className="login-container">
-      <img
-        src={window.location.origin + "/meta-mask.png"}
-        width="300px"
-        height="300px"
-      />
-      <button className="login-btn" type="submit" onClick={handleLogin}>
-        Login with metamask
-      </button>
-    </div>
+    <>
+      <div className="login-page">
+        {nameComp !== "" && ( // if the user input is shown, display the popup
+          <Popup
+            content={
+              <>
+                <b>MetaMask Fox: </b>
+                <p>
+                  Wow...you don't have an account on FLAMEAL yet. It's about the
+                  time you make one. Fill in your organisation's name and tick
+                  the box if you are a food bank. Be careful, you won't be able
+                  to change this information later. And don't lie...THE FOX
+                  KNOWS.
+                </p>
+              </>
+            }
+          />
+        )}
+
+        <div className="login-container">
+          <img
+            src={window.location.origin + "/meta-mask.png"}
+            width="300px"
+            height="300px"
+          />
+          {nameComp}
+
+          <button
+            className="login-btn"
+            type="submit"
+            onClick={() => {
+              handleLogin(orgName, isFoodBank); //call the login function and pass the name and FB state
+            }}
+          >
+            Login with metamask
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
