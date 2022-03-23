109a5b60faca2b67aacb8ba6a21c7be00b3b68d7
Lucian Duta <duta39@gmail.com>
2022-03-23 22:34:46 +0000

    Created a food bank card component

---
+++ client/src/components/FoodBankCard/FoodBankCard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./FoodBankCard.css";
+
+const FoodBankCard = ({ fbData }) => {
+  return (
+    <>
+      <div className="showcase-card">
+        <div className="card-image">
+          <img
+            src={fbData.pic || window.location.origin + "/food-bank.png"}
+            width="350px"
+            height="180px"
+          />
+        </div>
+        <div className="card-text">
+          <h1>{fbData.name}</h1>
+          <div className="card-description">{fbData.description}</div>
+        </div>
+        <div className="card-links">
+          <div className="card-links-address">
+            <h1>
+              Address:
+              {<p className="card-address">{fbData.address}</p>}
+            </h1>
+          </div>
+          <div className="card-links-trans">
+            <h1>Transactions: {"0"}</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FoodBankCard;
