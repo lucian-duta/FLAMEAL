814ff74b276cfbe8f1d21fe83a03c01393604b79
Lucian Duta <duta39@gmail.com>
2022-03-23 02:07:55 +0000

    Added component to display on page

---
+++ client/src/components/EditFoodBank/EditFoodBank.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./EditFoodBank.css";
+const EditFoodBank = () => {
+  return (
+    <>
+      <div className="edit-fb-container">Hello</div>
+    </>
+  );
+};
+
+export default EditFoodBank;
