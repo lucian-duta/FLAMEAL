80dc373f24733bb6b99a5ca3b377ac98a0be911e
Lucian Duta <duta39@gmail.com>
2022-03-24 00:24:22 +0000

    Created actions to get food bank and update

--- client/src/api/actions.js
+++ client/src/api/actions.js
@@ -47,6 +47,19 @@ export const createfb = (fbData) => {
       });
   });
 };
+export const updateFb = (fbData) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .post(`${urlfb}/updatefb`, fbData)
+      .then((res) => {
+        resolve(true);
+        console.log("foodbank updated", res);
+      })
+      .catch((e) => {
+        reject(false);
+      });
+  });
+};
 
 export const getfb = () => {
   return new Promise((resolve, reject) => {
@@ -58,6 +71,20 @@ export const getfb = () => {
       })
       .catch((e) => {
         reject([]);
+      });
+  });
+};
+
+export const getOneFb = (address) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(`${urlfb}/getonefb/${address}`)
+      .then((res) => {
+        resolve(res.data);
+        console.log("foodbanks: ", res.data);
+      })
+      .catch((e) => {
+        reject(null);
       });
   });
 };
