804130d40cfedd1a078c0e8700aa453b16d04cd6
Lucian Duta <duta39@gmail.com>
2022-04-01 18:59:25 +0100

    Added comments on getData

--- client/src/Web3/getData.js
+++ client/src/Web3/getData.js
@@ -20,10 +20,12 @@ const getData = () =>
     try {
       // Get network provider and web3 instance.
       const web3 = new Web3(window.ethereum);
-
+      //check is the instance is valid by testing provider
       if (!web3.currentProvider) {
+        //if the browser does not have metamask, ask fro another provider
         web3Elements.web3 = await getWeb3();
       } else {
+        //if metamask was detected, update the elements
         web3Elements.web3 = web3;
       }
 
