353cff899d998c7359934ded8d0bee2e6ecc998e
Lucian Duta <duta39@gmail.com>
2022-04-09 18:49:02 +0100

    Renaming/relocating login and topcont

---
+++ client/src/pages/TopCont.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TopContainer from "../components/TopContributors/TopContainer";
+
+/**
+ * The react component to display the top contributors of the network
+ * @borrows {@link TopContainer} as a child component to display the top contributors element
+ * @returns {ReactComponent} the top contributors component
+ */
+export default function TopCont() {
+  return (
+    <>
+      <TopContainer />
+    </>
+  );
+}
