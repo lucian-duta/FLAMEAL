2f5e68e70b1ae71bbd6abb51f18e8d2e173763c2
Lucian Duta <duta39@gmail.com>
2022-04-13 00:48:09 +0100

    Changed the way metamask instance is fetched

--- client/src/Web3/getWeb3.js
+++ client/src/Web3/getWeb3.js
@@ -17,29 +17,30 @@ const getWeb3 = () =>
       const web3 = new Web3(window.ethereum);
       try {
         // Request account access if needed
-        await window.ethereum.enable();
+        await window.ethereum
+          .enable()
+          .then(() => {
+            resolve(web3);
+            console.log("CONNECTED TO METAMASK");
+            metamaskConnected = true;
+          })
+          .catch((error) => {
+            reject(error);
+            console.log("FAILED TO CONNECT TO METAMASK");
+            metamaskConnected = false;
+          });
+
         // Accounts now exposed
-        resolve(web3);
-        console.log("CONNECTED TO METAMASK");
-        metamaskConnected = true;
       } catch (error) {
         console.log(error);
       }
     }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      // Use Mist/MetaMask's provider.
-      const web3 = window.web3;
-      console.log("Injected web3 detected.");
-      metamaskConnected = true;
-      resolve(web3);
-    }
     // Fallback to localhost; use dev console port by default...
     else {
-      const provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
-      const web3 = new Web3(provider);
-      console.log("No web3 instance injected, using Local web3.");
-      resolve(web3);
+      //const provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
+      //const web3 = new Web3(provider);
+      //console.log("No web3 instance injected, using Local web3.");
+      reject("No web3 instance injected, using Local web3.");
     }
   });
 
