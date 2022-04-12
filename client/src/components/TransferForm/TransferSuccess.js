7544f4ecc958f0562caa77b77ac06106e938b1b7
Lucian Duta <duta39@gmail.com>
2022-04-13 00:50:38 +0100

    Not refreshing in success page anymore

--- client/src/components/TransferForm/TransferSuccess.js
+++ client/src/components/TransferForm/TransferSuccess.js
@@ -6,14 +6,9 @@ import "./TransferForm.css";
  * @returns {React.Component} the component
  */
 const TransferSuccess = () => {
-  function timedRefresh(timeoutPeriod) {
-    setTimeout("location.reload(true);", timeoutPeriod);
-  }
-  //call the function after the page loads to refresh after 5 seconds
-  window.onload = timedRefresh(5000); //TODO make dependent on the metamask window
   return (
     <div className="form-content-right">
-      <h1 className="form-success">Transfer complete</h1>
+      <h1 className="form-success">Transfer processing</h1>
     </div>
   );
 };
