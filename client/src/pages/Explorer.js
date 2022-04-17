de85dcdfe3ab365ff3d31b07df7e6e9b97929536
Lucian Duta <duta39@gmail.com>
2022-04-17 19:13:12 +0100

    Created page component to display search as child

---
+++ client/src/pages/Explorer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SearchTransaction from "../components/SearchTransaction/SearchTransaction";
+const Explorer = () => {
+  return (
+    <div>
+      <SearchTransaction />
+    </div>
+  );
+};
+
+export default Explorer;
